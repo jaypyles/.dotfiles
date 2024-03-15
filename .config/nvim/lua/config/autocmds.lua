d9a11beeb2ce1edd673f8d600870be799832b26b
Jayden Pyles <jpylesbusiness@gmail.com>
2024-03-14 20:28:27 -0500

    test

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,10 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
--- disable format for filetype toml
+-- disable format for filetype
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "toml" },
-    callback = function()
-        vim.b.autoformat = false
-    end,
+  pattern = { "toml" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
 })
