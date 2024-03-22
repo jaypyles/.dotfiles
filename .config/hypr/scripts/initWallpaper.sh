d6dc250d5461fed841001be2c2171cb3d248728a
Jayden Pyles <jpylesbusiness@gmail.com>
2024-03-22 16:46:49 -0500

    Additions

---
+++ .config/hypr/scripts/initWallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PATH="${PATH}:${HOME}/.local/bin/"
+
+swww init
+
+DIR=$HOME/walls
+PICS=($(ls ${DIR}))
+
+RANDOMPICS=${PICS[$RANDOM % ${#PICS[@]}]}
+
+swww img ${DIR}/${RANDOMPICS}
+wal -i ${DIR}/${RANDOMPICS} --cols16 lighten
+
+echo "Successfully set a new wallpaper and generated colors from it."
