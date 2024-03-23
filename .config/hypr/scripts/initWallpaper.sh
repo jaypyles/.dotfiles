96cf1ee61bb104fc1236710032327daac71b13af
Jayden Pyles <jpylesbusiness@gmail.com>
2024-03-23 12:40:34 -0500

    additions

--- .config/hypr/scripts/initWallpaper.sh
+++ .config/hypr/scripts/initWallpaper.sh
@@ -14,4 +14,6 @@ wal -i ${DIR}/${RANDOMPICS} --cols16 lighten
 
 echo "Successfully set a new wallpaper and generated colors from it."
 
-waybar
+ps axf | grep waybar | grep -v grep | awk '{print "kill -9 " $1}' | sh # kill waybar
+waybar &
+./env/bin/python combine_yamls.py
